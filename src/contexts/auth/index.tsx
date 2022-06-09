cfdf261358a1df7b2b22d6e15d7d1f93039efbb4
Mael LAIGLE <mlaigle@veepee.com>
2022-06-09 17:16:02 +0200

    feat(auth): add a way to get userId from a context / hook

---
+++ src/contexts/auth/index.tsx
@@ -0,0 +1,28 @@
+import React, {createContext, ReactNode, useContext} from 'react'
+import {getLoggedUserId} from "@/utils/getLoggedUserId";
+
+type CtxProps = {
+    userId?: number | null
+}
+export const AuthContext = createContext<CtxProps>({
+    userId: null
+})
+
+type Props = {
+    children?: ReactNode
+    initialTranslations?: Record<string, string>
+}
+
+const AuthProvider: React.FC<Props> = ({ children }) => {
+    const userId = getLoggedUserId()
+
+    return (
+        <AuthContext.Provider value={{ userId }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => useContext(AuthContext)
+
+export default AuthProvider
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,17 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 
-import { getLoggedUserId } from '@/utils/getLoggedUserId'
+import AuthProvider from '@/contexts/auth'
 import theme from '@/themes'
 
 import '../styles/globals.css'
 
-// Default way to get a logged user
-export const loggedUserId = getLoggedUserId()
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+          <AuthProvider>
+              <Component {...pageProps} />
+          </AuthProvider>
       </ThemeProvider>
   )
 }
