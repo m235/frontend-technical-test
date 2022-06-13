5b5c63b71b21e4f6d5f88e27453c3ec65071a689
Mael LAIGLE <mlaigle@veepee.com>
2022-06-13 19:13:34 +0200

    feat: add basic auth context

--- src/contexts/auth/index.tsx
+++ src/contexts/auth/index.tsx
@@ -1,30 +1,35 @@
-import React, {createContext, ReactNode, useContext} from 'react'
+import { AxiosError } from 'axios'
+import React, { createContext, FC, ReactNode, useContext } from 'react'
+import { useQuery } from 'react-query'
 
-import { getLoggedUserId } from "@/utils/getLoggedUserId"
+import { getUser } from '@/services/users'
+import { User } from '@/types/user'
+import { getLoggedUserId } from '@/utils/getLoggedUserId'
 
 type CtxProps = {
-    userId?: number | null
+  userId?: number | null
+  user?: User
 }
 
 export const AuthContext = createContext<CtxProps>({
-    userId: null
+  userId: null,
+  user: null,
 })
 
 type Props = {
-    children?: ReactNode
-    initialTranslations?: Record<string, string>
+  children?: ReactNode
 }
 
-export let toto = ''
+const AuthProvider: FC<Props> = ({ children }) => {
+  const userId = getLoggedUserId()
 
-const AuthProvider: React.FC<Props> = ({ children }) => {
-    const userId = getLoggedUserId()
+  const { data } = useQuery<User, AxiosError>(['user', userId], async () => {
+    const { data } = await getUser(userId)
 
-    return (
-        <AuthContext.Provider  value={{ userId }}>
-            {children}
-        </AuthContext.Provider>
-    )
+    return data[0]
+  })
+
+  return <AuthContext.Provider value={{ userId, user: data }}>{children}</AuthContext.Provider>
 }
 
 export const useAuth = () => useContext(AuthContext)
