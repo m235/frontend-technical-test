5075d092d08c54093359d607dbeaa41e3989bc07
Mael LAIGLE <mlaigle@veepee.com>
2022-06-13 19:14:02 +0200

    feat: update _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,48 @@
 import type { AppProps } from 'next/app'
-import { QueryClientProvider } from 'react-query'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+import { useEffect, useState } from 'react'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import { ThemeProvider } from 'styled-components'
 
 import AuthProvider from '@/contexts/auth'
-import { queryClient } from '@/services/client'
 import theme from '@/themes'
-import '@/styles/globals.css'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import Layout from '@/components/layout'
+import GlobalStyle from '@/styles/globals'
+import NProgressStyle from '@/styles/nprogress'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  const [queryClient] = useState(() => new QueryClient())
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', NProgress.start)
+    router.events.on('routeChangeComplete', NProgress.done)
+    router.events.on('routeChangeError', NProgress.done)
+
+    return () => {
+      router.events.off('routeChangeStart', NProgress.start)
+      router.events.off('routeChangeComplete', NProgress.done)
+      router.events.off('routeChangeError', NProgress.done)
+    }
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </AuthProvider>
+      <GlobalStyle />
+      <NProgressStyle />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+          <AuthProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AuthProvider>
+        </Hydrate>
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
