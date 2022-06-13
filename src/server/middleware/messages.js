46f44b72470db9733ee7fbc0b0c8d40b6ed46baf
Mael LAIGLE <mlaigle@veepee.com>
2022-06-13 19:13:01 +0200

    feat: add a middleware for messages

--- src/server/middleware/messages.js
+++ src/server/middleware/messages.js
@@ -4,13 +4,16 @@ const db = require(`${path.dirname(__filename)}/../db.json`)
 // Need this middleware to catch some requests
 // and return both conversations where userId is sender or recipient
 module.exports = (req, res, next) => {
-  if (/conversations/.test(req.url) && req.method === 'GET') {
-    const userId = req.query?.senderId
+  if (/messages/.test(req.url) && req.method === 'GET') {
+    const last = req.query?.last === 'true'
+    if (last) {
+      const conversationId = req.query?.conversationId
 
-    const result = db?.conversations?.filter((conv) => conv.senderId == userId || conv.recipientId == userId)
+      const result = db?.messages?.filter((message) => message.conversationId == conversationId)?.at(-1)
 
-    res.status(200).json(result)
-    return
+      res.status(200).json(result)
+      return
+    }
   }
 
   next()
